c4d69106d7ccd350065185216585fb6c193e3c49
tifred <tifred@yunohost.tifred.nohost.me>
2014-10-07 09:32:39 +0200

    Almost ready to test

--- sources/index.php
+++ sources/index.php
@@ -1,3 +1,12 @@
 <?php
-print_r($_HEADER);
+
+$user = $_SERVER["REMOTE_USER"];
+$path = './port.number.ini';
+$ini = parse_ini_file($path);
+$port = $ini[$user];
+$url = "http://localhost:$port";
+print "<html><head></head><body><div id='content'>";
+include($url);
+print "</div></body></html>";
 ?>
+
